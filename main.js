c405a4ce20e325d3d9d4273786ef056e7cee544b
Badoni <badsu02@ca.com>
2017-06-11 12:39:39 +0530

    working

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 (function(window, document) {
-    
+
     'use strict';
 
     var model = {
         init: function () {
-            if(!localStorage.notes) {
+            if (!localStorage.notes) {
                 localStorage.notes = JSON.stringify([]);
             }
         },
-        save: function (note) {
+        add: function (note) {
             var notes = JSON.parse(localStorage.notes);
             notes.push(note);
+            localStorage.notes = JSON.stringify(notes);
         },
         getAll: function () {
             return JSON.parse(localStorage.notes);
@@ -19,12 +20,48 @@
 
     var view = {
         init: function () {
-            octupus.getNotes().array.forEach(function(item) {
-                
-            }, this);
+            var that = this;
+            document.addEventListener('DOMContentLoaded', function() {
+                var notesForm = document.querySelectorAll('#new-note')[0];
+                notesForm.addEventListener('submit', function(event) {
+                    event.preventDefault();
+                    octopus.saveNewNote(notesForm.name.value);
+                    notesForm.name.value = '';
+                    that.render();
+                });
+            });
+            this.render();
         },
         render: function() {
-
+            var notesList = document.querySelectorAll('#notes')[0];
+            while(notesList.hasChildNodes()) {
+                notesList.removeChild(notesList.firstChild);
+            }
+            octopus.getNotes().forEach(function(item) {
+                var noteListItem = document.createElement('li');
+                noteListItem.innerText = item;
+                notesList.appendChild(noteListItem);
+                this.fadeIn(noteListItem);
+            }, this);
+        },
+        fadeIn: function(element) {
+            element.className += ' fade-in';
         }
-    }
+    };
+
+    var octopus = {
+        init: function () {
+            model.init();
+            view.init();
+        },
+        getNotes: function() {
+            return model.getAll();
+        },
+        saveNewNote: function(note) {
+            model.add(note);
+        }
+    };
+
+    octopus.init();
+
 })(window, document);
